f3b3943423f4b287bc6533e247c67276cbfa5763
oghenekevbe <oghenekevbe.egume@gmail.com>
2023-06-01 16:30:08 +0100

    fixed the mongodb database

--- stock/settings.py
+++ stock/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'djongo', 
     'sp', 
 ]
 
@@ -85,10 +86,19 @@ WSGI_APPLICATION = 'stock.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'stockholders',
+        'MONGO_URI' : config('MONGO_URI')
     }
 }
 
